b3af8585eeeb7054dacddd39e39f11378186b80e
Uli__ <ulisescolin332@gmail.com>
2025-10-16 14:10:38 -0600

    Create Programa5.c

---
+++ Programa5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+ int enteroNumAlumnos = 5;
+ float realCalif = 0.0, realPromedio = 0.0;
+ printf("\tPromedio de calificaciones\n");
+ for (int indice = 0 ; indice < enteroNumAlumnos ; indice++)
+ {
+ printf("\nIngrese la calificación del alumn %d\n", indice+1);
+ scanf("%f",&realCalif);
+ realPromedio += realCalif;
+ }
+ printf("\nEl promedio de las calificaciones ingresadas es: %f\n",
+ realPromedio/enteroNumAlumnos);
+ r
