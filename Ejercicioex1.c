b0fe9d2420f56e8e0da50675e0de5645f4b09fd8
Uli__ <ulisescolin332@gmail.com>
2025-10-24 16:43:50 -0600

    Create Ejercicioex1.c

---
+++ Ejercicioex1.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main()
+
+{
+    int N, M, R;
+    
+    do
+    {
+        printf("Ingrese el valor de N: ");
+        scanf("%d", &N);
+        printf("Ingrese el valor de M: ");
+        scanf("%d", &M);
+    }
+    while (N<M || N<0 || M<0);
+    
+    R = N % M; 
+
+    while (R != 0)
+    {
+        N = M;
+        M = R;
+        R = N % M;
+    }
+    printf("\nEl MCD de ambos numeros es: %d", M);
+    return 0;
+}
