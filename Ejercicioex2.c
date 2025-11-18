f522b89ec450721949f01a68bb838623d9135b4d
Uli__ <ulisescolin332@gmail.com>
2025-11-18 14:04:27 -0600

    Update Ejercicioex2.c

--- Ejercicioex2.c
+++ Ejercicioex2.c
@@ -1,27 +1,29 @@
-/* Francisco Colín Ulises 
+/* Francisco Colín Ulises
 Práctica 8, ejercicio extra 2*/
 
 #include <stdio.h>
-#define MAX 10  
 
-int main()
+void main()
 {
-    int i=0, arr[MAX], n;   
-    
-    for (i; i<MAX; i++)
+    int arr[10];
+    int n = 0;
+    int i = 0;
+
+    for (i; i < 10; i = i + 1)
     {
         do
         {
-            printf("Ingrese un valor: ");
+            printf("Ingrese un valor par: ");
             scanf("%d", &n);
         }
         while (n % 2 != 0);
-        arr[i]=n;
+        arr[i] = n;
     }
-    i=0;
-    for (i; i<MAX; i++)
+
+    i = 0;
+
+    for (i; i < 10; i = i + 1)
     {
         printf("[%d] ", arr[i]);
     }
-    return 0;
 }
